fa6c381d81d6dd282fbf0ab5cee6a072c9fb3903
BozanaMa <71645909+BozanaMa@users.noreply.github.com>
2022-04-03 19:16:04 +0200

    LD3 ohne letzten Punkt

--- app/src/main/java/at/ac/fhcampuswien/movieappld02/widgets/MovieWidgets.kt
+++ app/src/main/java/at/ac/fhcampuswien/movieappld02/widgets/MovieWidgets.kt
@@ -3,11 +3,11 @@ package at.ac.fhcampuswien.movieappld02.widgets
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -17,11 +17,14 @@ import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import at.ac.fhcampuswien.movieappld02.models.Movie
 import at.ac.fhcampuswien.movieappld02.models.getMovies
-import coil.compose.rememberImagePainter
-import coil.transform.CircleCropTransformation
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 
 @ExperimentalAnimationApi
@@ -48,8 +51,22 @@ fun MovieRow(
                     .size(100.dp)
                     .padding(12.dp),
             ) {
+
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(movie.images[0])
+                        .crossfade(true)
+                        .build(),
+
+                contentDescription ="movie poster",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.clip(CircleShape)
+                )
+
+
                 //Icon(imageVector = Icons.Default.AccountBox, contentDescription = "movie icon")
 
+                /*
                 Image(
                     painter = rememberImagePainter(
                         data = movie.images[0],
@@ -60,6 +77,7 @@ fun MovieRow(
                     contentDescription = "Movie poster",
                     //modifier = Modifier.size(128.dp)
                 )
+                 */
             }
 
             var arrowInfo by remember {
@@ -133,9 +151,19 @@ fun HorizontalScrollableImageView(movie: Movie = getMovies()[0]) {
                 elevation = 4.dp
             ) {
 
-                Image(painter = rememberImagePainter(data = image),
-                    contentDescription = "movie image"
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(image)
+                        .crossfade(true)
+                        .build(),
+
+                    contentDescription ="movie image",
+
                 )
+
+                //Image(painter = rememberImagePainter(data = image),
+                  //  contentDescription = "movie image"
+                //)
             }
 
         }
